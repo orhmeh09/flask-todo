0f38cbde214f470ff83f92e2e93858626b9341fd
orhmeh09 <altan@imap.cc>
2019-02-02 15:44:58 -0800

    Initial

---
+++ FlaskTodo/__init__.py
@@ -0,0 +1,8 @@
+"""
+The flask application package. This is where we initialize Flask
+"""
+from flask import Flask
+app = Flask(__name__)
+wsgi_app = app.wsgi_app #Registering with IIS
+import FlaskTodo.todo
+
