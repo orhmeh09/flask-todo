f7fefeb578867d011ebcf7995f84d5b9185c7dc0
orhmeh09 <altan@imap.cc>
2019-02-02 16:45:30 -0800

    A

--- FlaskTodo/webapp.py
+++ FlaskTodo/webapp.py
@@ -1,8 +1,8 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command. 
-from . import Todo  # For import side-effects of setting up routes. 
+#from . import Todo  # For import side-effects of setting up routes. 
 
 # Time-saver: output a URL to the VS Code terminal so you can easily 
-Ctrl+click to open a browser
-# print('http://127.0.0.1:5000/hello/VSCode')
+#C#trl+click to open a browser
+## print('http://127.0.0.1:5000/hello/VSCode')
 
